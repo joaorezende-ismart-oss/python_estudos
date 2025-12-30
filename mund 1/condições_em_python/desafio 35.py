c39df1ee63c18aa85042eb5a0a7038ca9ec5d672
João rezende <joaovitor.rezende.ismart@gmail.com>
2025-12-30 11:27:13 -0300

    Update desafio 35.py

--- mund 1/condições_em_python/desafio 35.py
+++ mund 1/condições_em_python/desafio 35.py
@@ -1,3 +1,4 @@
+from time import sleep
 r1 = float(input('Valor da reta: ')) #a
 r2 = float(input('Valor da segunda reta: ')) #b
 r3 = float(input('Vlor da terceira reta: ')) #c
@@ -7,6 +8,7 @@ if d < 0:
 triangulo = d < r1 < r2 + r3
 if triangulo == True:
     print('De acordo com as medias da reta...')
+    sleep(2)
     print('É possivel criar um triangulo')
 else:
     print('Não é possivel criar um triangulo com essas medidas')
