c9d83990b50bdd035c0371b2c47161d3637701bd
João rezende <joaovitor.rezende.ismart@gmail.com>
2025-12-30 10:36:50 -0300

    Update desafio 32.py

--- mund 1/condições_em_python/desafio 32.py
+++ mund 1/condições_em_python/desafio 32.py
@@ -1,7 +1,7 @@
-print('Vamos vamos ver se tal ano é bisssesto')
+print('Vamos vamos ver se tal ano é bisssexto')
 ano = int(input('Digite um ano qualquer: '))
 ano_bissesto = ano % 4 
 if ano_bissesto == 0:
-    print('O ano de {} é um ano bissesto, ou seja contem 366 dias'.format(ano))
+    print('O ano de {} é um ano bissexto, ou seja contem 366 dias'.format(ano))
 else:
-    print('O ano de {} não é bissesto'.format(ano))
+    print('O ano de {} não é bissexto'.format(ano))
