778f1fa7c589823a3bd191e2ddcd7041c84bc377
João rezende <joaovitor.rezende.ismart@gmail.com>
2025-12-31 18:56:51 -0300

    Update desafio 6 v2.py

--- mund 1/adicionando_cores_ao_python/desafio 6 v2.py
+++ mund 1/adicionando_cores_ao_python/desafio 6 v2.py
@@ -1,7 +1,11 @@
 n1 = int(input('Escolha um numero: '))
+
 s = n1 + 1
 su = n1 - 1
+
 print('Você sabia que o sucessor do \033[31m{}\033[m é o \033[32m{}\033[m e o antecessor é o \033[32m{}\033[m'.format(n1, s, su))
 print('=' * 20)
+
 n2 = int(input('Escolha outro numero: '))
+
 print('Seu antecessor é \033[32m{}\033[m e seu sucessor é \033[32m{}\033[m'.format(n2 - 1, n2 + 1))
